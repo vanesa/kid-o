633e3672c3f6f0200e45167ad5dc7608ef7f9e93
Vanesa <vanesa@gmx.net>
2015-09-11 18:39:22 -0700

    Use Heroku post & open interface

--- run.py
+++ run.py
@@ -10,10 +10,11 @@ if __name__ == '__main__':
 
     # debug = True as DebugToolbarExtension is invoked
 
-    app.debug = True
     connect_to_db(app)
 
     # User the DebugToolbar
     # DebugToolbarExtension(app)
 
-    app.run()
+	PORT = int(os.environ.get("PORT", 5000))
+
+	app.run(debug=True, host="0.0.0.0", port=PORT)

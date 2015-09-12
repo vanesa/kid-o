6d68d07f30f2244b13207c6eaf9d4662492b04e2
Vanesa <vanesa@gmx.net>
2015-09-11 18:42:58 -0700

    Change indentation tab to spaces.

--- run.py
+++ run.py
@@ -15,6 +15,6 @@ if __name__ == '__main__':
     # User the DebugToolbar
     # DebugToolbarExtension(app)
 
-	PORT = int(os.environ.get("PORT", 5000))
+    PORT = int(os.environ.get("PORT", 5000))
 
-	app.run(debug=True, host="0.0.0.0", port=PORT)
+    app.run(debug=True, host="0.0.0.0", port=PORT)

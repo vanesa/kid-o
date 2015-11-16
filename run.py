76b25b7f46aca3d83ce2765bd632c72b1fa3ae5b
Vanesa <vanesa@gmx.net>
2015-11-15 16:34:08 -0800

    heroku modifications

--- run.py
+++ run.py
@@ -16,5 +16,5 @@ if __name__ == '__main__':
     # User the DebugToolbar
     # DebugToolbarExtension(app)
     PORT = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=PORT)
-	debug = False if os.environ.get()
+    app.run(debug=False, host="0.0.0.0", port=PORT)
+	# debug = False if os.environ.get()

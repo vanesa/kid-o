31573cd15ebf7a5c7818812d2d585f7e6a61bbbc
Vanesa <vanesa@gmx.net>
2015-11-15 16:35:44 -0800

    heroku modifications

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 from .models import db
 
 app.config.from_object('app.settings')
-app.config['DATABASE_URL'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
 
 # Required to use Flask sessions and debug toolbar
 

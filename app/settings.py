2f5f3f9521464a0619cf61fa9b112ce5e885dee5
Vanesa <vanesa@gmx.net>
2015-12-10 10:51:39 -0800

    fix travis db config error

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,8 @@
+import os
+
 UPLOAD_FOLDER = 'static/images/photos/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 WTF_CSRF_ENABLED = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/kid-o'
+IS_TRAVIS = os.environ.get('IS_TRAVIS') == 'true'
+DB_NAME = 'kid-o' if not IS_TRAVIS else 'travis_ci_test'
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/' + DB_NAME

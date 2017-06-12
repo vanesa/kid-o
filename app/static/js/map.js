b792134b5c34a6aabf72bc1eb125cdfd5253ec73
Vanesa <vanesa@gmx.net>
2017-06-11 18:41:43 -0700

    retrieve children profiles from api for map page

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,97 +1,85 @@
 $(function() {
-  L.mapbox.accessToken = 'pk.eyJ1IjoidmFuZXNhIiwiYSI6ImYxOTAxOGI1NTBlOGJkMTdjZTRmNGVmNTg0NTUxMjFjIn0._G3yYtIvkPX1EC9QEkNB6Q';
-  
-  var geojson = [];
+	$.getJSON("/api/children_profiles",function (data) {
+	  L.mapbox.accessToken = 'pk.eyJ1IjoidmFuZXNhIiwiYSI6ImYxOTAxOGI1NTBlOGJkMTdjZTRmNGVmNTg0NTUxMjFjIn0._G3yYtIvkPX1EC9QEkNB6Q';
+	  
+	  var geojson = [];
 
-  for (child in window.children) {
-  	child = window.children[child];
-  	if (child.photo_url == '/static/images/childphotopreview.png') {
-  		var child_photo = '/static/images/photos/childphotopreview.png';
-  	}
-  	else if (child.photo_url.indexOf(child.first_name.toLowerCase()) !== -1) {
-  		var child_photo = child.photo_url;
-  	}
-  	else {
-  		var child_photo = '/static/images/photos/childphotopreview.png';
-  	}
+	  for (child in data.profiles) {
+	  	child = data.profiles[child];
 
-  	var description = 'lives here '
-  	if (child.guardian_fname || child.siblings_in_project) {
-  		description += 'with ';
-  	}
-  	else if (child.guardian_fname) {
-			description.append(child.guardian_fname + ' ' + child.guardian_lname + ' ');
-  	}
-  	else if (child.siblings_in_project) {
-			description.append(child.siblings_in_project);
-  	}
+	  	if (child.photo_url == '/static/images/childphotopreview.png') {
+	  		var child_photo = '/static/images/photos/childphotopreview.png';
+	  	}
+	  	else if (child.photo_url.indexOf(child.first_name.toLowerCase()) !== -1) {
+	  		var child_photo = child.photo_url;
+	  	}
+	  	else {
+	  		var child_photo = '/static/images/photos/childphotopreview.png';
+	  	}
 
-		var child_geo = {
-	    'type': 'Feature',
-	    'geometry': {
-	    'type': 'Point',
-	    'coordinates': [child.longitude, child.latitude]
-	    },
-	    'properties': {
-	  	'image': child_photo,
-	  	'title': child.first_name + ' ' + child.last_name,
-	  	'url': '/child/' + child.id,
-			'description': description,
-			'marker-color': '#fc4353',
-			'marker-size': 'large',
-			'marker-symbol': 'building'
-			}
-		};
+	  	var description = 'lives here '
+	  	if (child.guardian_fname || child.siblings_in_project) {
+	  		description += 'with ';
+	  	}
+	  	else if (child.guardian_fname) {
+				description.append(child.guardian_fname + ' ' + child.guardian_lname + ' ');
+	  	}
+	  	else if (child.siblings_in_project) {
+				description.append(child.siblings_in_project);
+	  	}
 
-	geojson.push(child_geo);
+	  	if (!child.longitude) {
+	  		child.longitude = 18.542769;
+      	child.latitude = -69.801216;
+	  	}
 
-  }
+			var child_geo = {
+		    'type': 'Feature',
+		    'geometry': {
+		    'type': 'Point',
+		    'coordinates': [child.longitude, child.latitude]
+		    },
+		    'properties': {
+		  	'image': child_photo,
+		  	'title': child.first_name + ' ' + child.last_name,
+		  	'url': '/child/' + child.id,
+				'description': description,
+				'marker-color': '#fc4353',
+				'marker-size': 'large',
+				'marker-symbol': 'building'
+				}
+			};
 
+		geojson.push(child_geo);
 
-//   var geojson = [
-//   // Loop through each child and create marker and tooltip containing the information available
-//   {% for child in child_profiles %}
-//     {% if child.latitude and child.longitude %}
-//       {
-//       'type': 'Feature',
-//       'geometry': {
-//       'type': 'Point',
-//       'coordinates': [{{ child.longitude }},{{ child.latitude }}]
-//       },
-//       'properties': {
-//     'image': '/static/images/photos/' + {% if child.photo_url == '/static/images/childphotopreview.png' %}'childphotopreview.png'{% elif child.photo_url == '/static/images/photos/' + child.first_name.lower() + '.jpg' %}'{{ child.first_name }}'.toLowerCase() + '.jpg' {% else %} 'childphotopreview.png'{% endif %},
-//     'title': '{{ child.first_name }} {{ child.last_name }}',
-//     'url': '/child/{{ child.id }}',
-//   'description': 'lives here {% if child.guardian_fname and child.guardian_lname or child.siblings_in_project %} with {{ child.guardian_fname }} {{ child.guardian_lname }} {% if child.guardian_type %}({{ child.guardian_type }}){% else %} {% endif %} {{ child.siblings_in_project}} {% else %}{% endif %}',
-//   'marker-color': '#fc4353',
-//   'marker-size': 'large',
-//   'marker-symbol': 'building'
-//   }
-// },{% endif %}{% endfor %}
-// ];
+	  }
 
-var map = L.mapbox.map('map', 'vanesa.e4c935ef', {
-  scrollWheelZoom: false
-})
-.setView([18.542769, -69.801216], 15)
-.featureLayer;
-// Add custom feature to the tooltip
-map.on('layeradd', function(e) {
-  var marker = e.layer,
-  feature = marker.feature;
+		var map = L.mapbox.map('map', 'vanesa.e4c935ef', {
+		  scrollWheelZoom: false
+		})
+		.setView([18.542769, -69.801216], 15)
+		.featureLayer;
+		// Add custom feature to the tooltip
+		map.on('layeradd', function(e) {
+		  var marker = e.layer,
+		  feature = marker.feature;
 
-  // Create custom popup content
-  var popupContent =  '<div class="popupleft"><a href="' + feature.properties.url + '">' +
-      '<img class="popupimage" src="' + feature.properties.image + '" />' + '</div>' + '<div class="popupright">' +
-      '<a href="' + feature.properties.url + '">' + feature.properties.title +
-      '</a> ' + feature.properties.description + '</div>';
+		  // Create custom popup content
+		  var popupContent =  '<div class="popupleft"><a href="' + feature.properties.url + '">' +
+		      '<img class="popupimage" src="' + feature.properties.image + '" />' + '</div>' + '<div class="popupright">' +
+		      '<a href="' + feature.properties.url + '">' + feature.properties.title +
+		      '</a> ' + feature.properties.description + '</div>';
 
-    // http://leafletjs.com/reference.html#popup
-    marker.bindPopup(popupContent,{
-      closeButton: false,
-      minWidth: 180
-      });
-      });
+		  // http://leafletjs.com/reference.html#popup
+		  marker.bindPopup(popupContent,{
+		    closeButton: false,
+		    minWidth: 180
+		  });
+		});
 
-      map.setGeoJSON(geojson);
+		map.setGeoJSON(geojson);
+	})
+  .fail(function() {
+    $("#map").html("Error retrieving the children's profiles!").addClass("maperror animated1");
+  });
 });
